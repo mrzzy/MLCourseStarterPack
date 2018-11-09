54d470217290b5d46a4c75da84a4425a2e56b70a
Zhu Zhan Yan <program.nom@gmail.com>
2018-11-09 11:29:05 +0800

    Added additional data to improve model performance

--- matcher.py
+++ matcher.py
@@ -4,19 +4,19 @@
 # Session 1  - Case Study 2
 # Train Sementic Text matcher
 #
-import sys
 
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
+import pickle
+import sys
+from random import random, randint
 
 from sklearn.svm import SVR
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-from random import random, randint
 if __name__ == "__main__":
     # Load the simliarity data - data is tab seperated
     similarity_df =  pd.read_csv("data/similarity.tsv", sep="\t")
@@ -42,34 +42,44 @@ if __name__ == "__main__":
     train_vecs = vectorizer.transform(train_sents)
     test_vecs = vectorizer.transform(test_sents)
     
-    # Feature scaling to accelerate learning
-    scaler = StandardScaler(with_mean=False)
-    scaler.fit(train_vecs)
-    train_vecs = scaler.transform(train_vecs)
-    test_vecs = scaler.transform(test_vecs)
-        
     # Train the models with random hyperparameters, saving the best model
-    Cs = [ random() * 10 ** randint(-2, 3) for i in range(8) ]
-    gammas = [ random() * 10 ** randint(-5, 1) for i in range(6) ]
+    #best_model = None
+    #best_cost = 9e+9
+    #
+    #for i in range(60):
+    #    # Randomly select hyperparameters
+    #    c_val = random() * 10 ** randint(-3, 6)
+    #    gamma_val = random() * 10 ** randint(-5, 1)
 
-    best_model = None
-    best_cost = 9e+9
+    #    # Train model with random hyperparameter
+    #    model = SVR(C=c_val, gamma=gamma_val)
+    #    model.fit(train_vecs, train_scores)
+    #    
+    #    # Cross validate with test set by computing cost
+    #    predict_scores = model.predict(test_vecs)
+    #    cost = mean_squared_error(test_scores, predict_scores)
+    #    
+    #    # Display a dot every model trained
+    #    sys.stdout.flush()
+    #    print(".", end="")
+
+    #    # Save model if best model so far
+    #    if cost < best_cost:
+    #        print("Found better model: C={:.2e}, gamma={:.2e}".format(c_val, gamma_val))
+    #        print("cost: ", cost)
+    #        best_model = model
+    #        best_cost = cost
+    best_model = SVR(C=7.59e+02, gamma=4.82e-02)
+    best_model.fit(train_vecs, train_scores)
+    predict_scores = best_model.predict(test_vecs)
+    cost = mean_squared_error(test_scores, predict_scores)
+    print(cost)
+
+    # Save trained matcher pipeline: vectorizer, model for use later
+    pipeline = {
+        "vectorizer": vectorizer,
+        "model": best_model
+    }
     
-    for c_val in Cs:
-        for gamma_val in gammas:
-            # Train model with random hyperparameter
-            model = SVR(C=c_val, gamma=gamma_val)
-            model.fit(train_vecs, train_scores)
-            
-            # Cross validate with test set by computing cost
-            predict_scores = model.predict(test_vecs)
-            cost = mean_squared_error(test_scores, predict_scores)
-            sys.stdout.flush()
-
-            # Save model if best model so far
-            if cost < best_cost:
-                print("Found better model: C={:.2e}, gamma={:.2e}".format(c_val, gamma_val))
-                print("cost: ", cost)
-                best_model = model
-                best_cost = cost
-
+    with open("matcher.pickle", "wb") as f:
+        pickle.dump(pipeline, f)
